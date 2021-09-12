29884df2fc6ef5d444b47d45f858d54ccabc7a3a
Leonardo <leozikasantos73@gmail.com>
2021-09-12 00:48:52 -0300

    feat: add data dynamically to the table

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import Table from './components/Table';
 import './styles/global.css';
 
 const App: React.FC = () => {
-  return <Table />;
+  const [users, setUsers] = useState(null);
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?page=1&results=50')
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data.results);
+        console.log(users);
+      });
+  }, []);
+  if (users) return <Table data={users} />;
+  else return <div>Loading</div>;
 };
 
 export default App;
--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,40 +1,47 @@
 import React from 'react';
 import './styles.css';
 
-const Table: React.FC = () => {
+interface User {
+  email: string;
+  phone: string;
+  name: {
+    first: string;
+    last: string;
+  };
+  registered: {
+    date: string;
+    age: number;
+  };
+}
+
+interface TableProps {
+  data: Array<User>;
+}
+
+const Table: React.FC<TableProps> = ({ data }) => {
   return (
-    <table id="users">
-      <tr>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Age</th>
-        <th>Phone</th>
-      </tr>
-      <tr>
-        <td>Alfreds Futterkiste</td>
-        <td>Alf@gmail.com</td>
-        <td>18</td>
-        <td>99999999</td>
-      </tr>
-      <tr>
-        <td>Alfreds Futterkiste</td>
-        <td>Alf@gmail.com</td>
-        <td>18</td>
-        <td>99999999</td>
-      </tr>
-      <tr>
-        <td>Alfreds Futterkiste</td>
-        <td>Alf@gmail.com</td>
-        <td>18</td>
-        <td>99999999</td>
-      </tr>
-      <tr>
-        <td>Alfreds Futterkiste</td>
-        <td>Alf@gmail.com</td>
-        <td>18</td>
-        <td>99999999</td>
-      </tr>
-    </table>
+    <div className="tableContainer">
+      <table id="users">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Age</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name.first}</td>
+              <td>{item.email}</td>
+              <td>{item.registered.age}</td>
+              <td>{item.phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
